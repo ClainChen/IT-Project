d1dcc0f141dd7e41441f24bbe8de03fa568fb233
chencc3 <clain0719@gmail.com>
2023-08-21 18:41:11 +1000

    Update a Sound Folder

--- DOT-Interaction/Assets/Script/Animations/LineGenerationAnimation.cs
+++ DOT-Interaction/Assets/Script/Animations/LineGenerationAnimation.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using DOT.Animations;
 using UnityEngine;
+using DOT.Utilities;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    private GameObject[] dots;
+
     // Start is called before the first frame update
     void Start()
     {
