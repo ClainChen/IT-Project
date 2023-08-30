6490af017b9477502358ed9338ef61b4590ccb53
chencc3 <clain0719@gmail.com>
2023-08-30 21:20:28 +1000

    Update some Scene

--- DOT-Interaction/Assets/Script/ObjectGetter.cs
+++ DOT-Interaction/Assets/Script/ObjectGetter.cs
@@ -7,29 +7,34 @@ namespace DOT
 {
     public class ObjectGetter : MonoBehaviour
     {
-        [HideInInspector] public static GameObject lineLeft;
-        [HideInInspector] public static GameObject lineRight;
+        public static GameObject lineLeft;
+        public static GameObject lineRight;
 
-        [HideInInspector] public static GameObject matrixLeft;
-        [HideInInspector] public static GameObject matrixRight;
+        public static GameObject matrixLeft;
+        public static GameObject matrixRight;
 
-        [HideInInspector] public static GameObject regionLeft;
-        [HideInInspector] public static GameObject regionRight;
+        public static GameObject regionLeft;
+        public static GameObject regionRight;
 
-        [HideInInspector] public static List<GameObject> dotsLeft;
-        [HideInInspector] public static List<GameObject> dotsRight;
+        public static List<GameObject> dotsLeft;
+        public static List<GameObject> dotsRight;
 
         // Start is called before the first frame update
         void Awake()
         {
             lineLeft = GameObject.Find("LineLeft");
             lineRight = GameObject.Find("LineRight");
+
             matrixLeft = GameObject.Find("Left");
             matrixRight = GameObject.Find("Right");
+
             regionLeft = GameObject.Find("RegionLeft");
             regionRight = GameObject.Find("RegionRight");
+
             dotsLeft = GameObject.FindGameObjectsWithTag("Matrix1").ToList();
             dotsRight = GameObject.FindGameObjectsWithTag("Matrix2").ToList();
+
+            Debug.Log("Here!");
         }
     }
 }
